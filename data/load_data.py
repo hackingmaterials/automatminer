0b7b0415371ba034ec097531bfe14b9894574b9f
Alireza <alireza@wustl.edu>
2018-05-16 13:37:22 -0700

    cleanup

--- data/load_data.py
+++ data/load_data.py
@@ -1,25 +1,35 @@
 import pandas as pd
 
 """
-All load* methods load the data in pandas.DataFrame
+All load* methods return the data in pandas.DataFrame
 """
 
-def load_double_perovskites_gap(return_lumos=False):
+def load_double_perovskites_gap(return_lumo=False):
     """
-    1325??? electronic band gaps (in eV) calculated via ??? in GPAW. This data is used
-    in the following references
+    Electronic band gaps of double perovskites calculated using ﻿Gritsenko,
+    van Leeuwen, van Lenthe and Baerends potential in GPAW.
 
     Args:
-        return_lumos (bool): whether to return the lowest unoccupied molecular
+        return_lumo (bool): whether to return the lowest unoccupied molecular
             orbital (LUMO) energy levels (in eV).
-            
+
     References:
         1) https://www.nature.com/articles/srep19375
         2) CMR database: https://cmr.fysik.dtu.dk/
     """
-    df = pd.read_excel('data_files/double_perovskites.xlsx', sheet_name='gaps')
-    lumos = pd.read_excel('data_files/double_perovskites.xlsx', sheet_name='lumos')
-    if return_lumos:
-        return df, lumos
+    df = pd.read_excel('data_files/double_perovskites.xlsx', sheet_name='bandgap')
+    lumo = pd.read_excel('data_files/double_perovskites.xlsx', sheet_name='lumo')
+    if return_lumo:
+        return df, lumo
     else:
         return df
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(load_double_perovskites_gap().head())
