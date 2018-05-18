44e1a424348a867ef073bd356a59d2330aa1cb67
ardunn <ardunn@lbl.gov>
2018-05-18 15:02:03 -0700

    Added wolverton and m2ax load funcs

--- data/load_data.py
+++ data/load_data.py
@@ -40,13 +40,54 @@ def load_mp_data(filename='data_files/mp_nostruct.csv'):
     df = df.drop("mpid", axis=1)
     if 'structure' in df.columns.values:
         df['structure'] = df['structure'].map(ast.literal_eval).map(Structure.from_dict)
-    df = df.rename(columns={'material_id': 'mpid', 'pretty_formula': 'formula',
-                            'band_gap': 'gap pbe', 'e_above_hull': 'ehull',
-                            'elasticity.K_VRH': 'bulkmod',
-                            'elasticity.G_VRH': 'shearmod',
-                            'elasticity.elastic_anisotropy': 'elastic_anisotropy'})
-    return df
+    colmap = {'material_id': 'mpid',
+              'pretty_formula': 'formula',
+              'band_gap': 'gap pbe',
+              'e_above_hull': 'ehull',
+              'elasticity.K_VRH': 'bulkmod',
+              'elasticity.G_VRH': 'shearmod',
+              'elasticity.elastic_anisotropy': 'elastic_anisotropy',
+              'total_magnetization': 'mu_B'}
+    return df.rename(columns=colmap)
+
+def load_wolverton_oxides():
+    """
+    Wolverton's perovskite oxides containing composition data, lattice constants,
+    and formation + vacancy formation energies. There are 5,329 compounds
+    in this dataset.
+
+    From https://www.nature.com/articles/sdata2017153#ref40
+    """
+    df = pd.read_csv("data_files/wolverton_oxides.csv")
+    colmap = {"Chemical formula": "formula",
+              "A": "atom A",
+              "B": "atom B",
+              "Formation energy [eV/atom]": "eformation",
+              "Band gap [eV]": "gap pbe",
+              "Magnetic moment [mu_B]": "mu_B",
+              "Vacancy energy [eV/O atom]": "eformation oxygen vacancy",
+              "Stability [eV/atom]": "ehull"}
+    return df.rename(columns=colmap)
+
+
+def load_m2ax():
+    """
+    An elastic dataset of 224 stable M2AX compounds from "A comprehensive survey
+    of M2AX phase elastic properties" by Cover et al. Calculations are PAW
+    PW91.
+
+    From http://iopscience.iop.org/article/10.1088/0953-8984/21/30/305403/meta
+    """
+    df = pd.read_csv("data_files/m2ax_elastic.csv")
+    colmap = {"M2AX phase": "formula",
+              "B": "bulkmod",
+              "G": "shearmod",
+              "E": "elasticmod"}
+    return df.rename(columns=colmap)
 
 if __name__ == "__main__":
     # print(load_double_perovskites_gap().head())
-    print(load_mp_data())
+    # print(load_wolverton_oxides())
+    load_m2ax()
+    # print(load_mp_data())
+
