23134fb19a46c4e34783f4a1d995448b3c960281
Alireza <alireza.faghaninia@gmail.com>
2018-05-29 14:01:46 -0700

    start visualize

---
+++ matbench/visualize.py
@@ -0,0 +1,38 @@
+from matbench.data.load_data import load_double_perovskites_gap
+from matbench.prepare import PrepareData
+
+
+class VisualizeData(object):
+    """
+    Takes in a featurized dataframe (e.g. output of PrepareData) and provide
+    convenient tools to visualize the most relevant features and their
+    relationship.
+    """
+    def __init__(self, df, target_df=None):
+        if target_df:
+            df = df.concat(target_df)
+        self.df = df
+
+
+    def targetted_visualize(self, target, ncols=10):
+        """
+        Visualize the top few features that correlate with the target feature
+
+        Args:
+            target (str): target column to be predicted/studied
+
+        Returns:
+        """
+        print(self.df.corr)
+
+
+if __name__ == "__main__":
+    df_init, lumos = load_double_perovskites_gap(return_lumo=True)
+    prep = PrepareData(df_init,
+                       target_cols=['gap gllbsc'],
+                       ignore_cols=['A1', 'A2', 'B1', 'B2'])
+    prep.featurize_columns()
+    # prep.handle_nulls()
+
+    vis = VisualizeData(prep.get_train_target())
+    vis.targetted_visualize(target='gap gllbsc')
