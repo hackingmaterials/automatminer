45deab5e3153a6191ac4c3fa3fbd1ab8b21cbdc8
Alireza <alireza.faghaninia@gmail.com>
2018-05-29 09:53:17 -0700

    add setup for matbench

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+import multiprocessing, logging
+
+module_dir = os.path.dirname(os.path.abspath(__file__))
+
+if __name__ == "__main__":
+    setup(
+        name='matbench',
+        version='0.1.0',
+        description='benchmarking of machine learning models in material science',
+        long_description=open(os.path.join(module_dir, 'README.md')).read(),
+        url='https://github.com/hackingmaterials/matbench',
+        author=['Alireza Faghaninia', 'Alex Dunn', 'Qi Wang'],
+        author_email=['alireza.faghaninia@gmail.com', 'denhaus@gmail.com', 'qwang3@lbl.gov'],
+        license='modified BSD',
+        packages=find_packages(),
+        package_data={},
+        zip_safe=False,
+        install_requires=[],
+        extras_require={},
+        classifiers=['Programming Language :: Python :: 3.6',
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Science/Research',
+                     'Intended Audience :: System Administrators',
+                     'Intended Audience :: Information Technology',
+                     'Operating System :: OS Independent',
+                     'Topic :: Other/Nonlisted Topic',
+                     'Topic :: Scientific/Engineering'],
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        scripts=[]
+    )
