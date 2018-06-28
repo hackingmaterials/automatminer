c335a354cd64b57cf48a704bee1085aafda42a3d
Alireza <alireza.faghaninia@gmail.com>
2018-06-28 14:56:03 -0700

    add docs + better handling of input/output dataframes

--- matbench/featurize.py
+++ matbench/featurize.py
@@ -10,8 +10,16 @@ from warnings import warn
 class Featurize(object):
     """
     Takes in a dataframe and generate features from preset columns such as
-    "formula", "structure", "bandstructure", "dos", etc.
-
+    "formula", "structure", "bandstructure", "dos", etc. One may use
+    the featurize_columns method to featurize via all available featurizers
+    with default setting or selectively call featurizer methods.
+    Usage examples:
+        featurizer = Featurize(df)
+            df = featurizer.featurize_columns() # all features of all types
+        or:
+            df = featurizer.featurize_formula() # all formula-related feature
+        or:
+            df = featurizer.
     Args:
         df (pandas.DataFrame): the input data containing at least one of preset
             inputs (e.g. "formula")
@@ -20,7 +28,7 @@ class Featurize(object):
     def __init__(self, df, ignore_cols=None):
         self.df = df if ignore_cols is None else df.drop(ignore_cols, axis=1)
 
-    def featurize_columns(self, input_cols=None):
+    def featurize_columns(self, df=None, input_cols=None):
         """
         Featurizes the dataframe based on input_columns.
 
@@ -31,7 +39,8 @@ class Featurize(object):
         Returns (pandas.DataFrame):
             self.df w/ new features added via featurizering input_cols
         """
-        df = self.df.copy(deep=True)
+        if df is None:
+            df = self.df.copy(deep=True)
         input_cols = input_cols or ["formula"]
         for column in input_cols:
             featurizer = getattr(self, "featurize_{}".format(column), None)
@@ -44,22 +53,29 @@ class Featurize(object):
         return df
 
     #TODO: -AF see if the only use of featurize_* methods is to be called in featurize_columns, think about defining them outside of the class
-    @staticmethod
-    def featurize_formula(df, col_id="formula",
-                          preset_name="matminer", compcol="composition"):
+    def featurize_formula(self, df=None, featurizers='all', col_id="formula",
+                          preset_name="matminer", compcol="composition",
+                          ignore_errors=True):
+        if df is None:
+            df = self.df.copy(deep=True)
         if compcol not in df:
             df[compcol] = df[col_id].apply(Composition)
-        featurizer = MultipleFeaturizer([
-            cf.ElementProperty.from_preset(preset_name=preset_name),
-            cf.IonProperty()
-        ])
-        df = featurizer.featurize_dataframe(df, col_id=compcol)
-        df = df.drop([compcol], axis=1)
+        if featurizers=='all':
+            featurizer = MultipleFeaturizer([
+                cf.ElementProperty.from_preset(preset_name=preset_name),
+                cf.IonProperty()
+            ])
+        else:
+            featurizer = MultipleFeaturizer(featurizers)
+        df = featurizer.featurize_dataframe(df,
+                                            col_id=compcol,
+                                            ignore_errors=ignore_errors)
         return df
 
 
-    @staticmethod
-    def featurize_structure(df, col_id="structure", preset_name="ops"):
+    def featurize_structure(self, df=None, col_id="structure", preset_name="ops"):
+        if df is None:
+            df = self.df.copy(deep=True)
         featurizer = MultipleFeaturizer([
             sf.SiteStatsFingerprint(
                 site_featurizer=sf.CrystalSiteFingerprint.from_preset(
@@ -68,13 +84,17 @@ class Featurize(object):
             sf.DensityFeatures(),
             sf.GlobalSymmetryFeatures()
         ])
-        df = featurizer.featurize_dataframe(col_id=col_id)
+        df = featurizer.featurize_dataframe(df, col_id=col_id)
         return df
 
 
 if __name__ == "__main__":
     df_init = load_double_perovskites_gap(return_lumo=False)
-    prep = Featurize(df_init,
+    featurizer = Featurize(df_init,
                      ignore_cols=['a_1', 'a_2', 'b_1', 'b_2'])
-    df = prep.featurize_columns()
+    df = featurizer.featurize_columns()
+    # df = featurizer.featurize_formula(df_init, featurizers='all')
+
     print(df.head())
+    print('The original df')
+    print(featurizer.df)
