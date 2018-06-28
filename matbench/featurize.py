dbdb140dbee6168c601a2d8c1783d180aeaf28a0
Alireza <alireza.faghaninia@gmail.com>
2018-06-28 16:42:46 -0700

    add structure to AllFeaturizers + CationProperty + guessoxidstate arg

--- matbench/featurize.py
+++ matbench/featurize.py
@@ -1,6 +1,7 @@
 from matminer.featurizers.base import MultipleFeaturizer
 import matminer.featurizers.composition as cf
 import matminer.featurizers.structure as sf
+from matminer.utils.conversions import composition_to_oxidcomposition
 from pymatgen import Composition
 from matbench.data.load import load_double_perovskites_gap
 from matbench.utils.utils import MatbenchError
@@ -25,9 +26,11 @@ class Featurize(object):
             inputs (e.g. "formula")
         ignore_cols ([str]): if set, these columns are excluded
     """
-    def __init__(self, df, ignore_cols=None, preset_name="matminer"):
+    def __init__(self, df, ignore_cols=None, preset_name="matminer",
+                 ignore_errors=True):
         self.df = df if ignore_cols is None else df.drop(ignore_cols, axis=1)
         self.all_featurizers = AllFeaturizers(preset_name=preset_name)
+        self.ignore_errors = ignore_errors
 
     def featurize_columns(self, df=None, input_cols=None):
         """
@@ -53,56 +56,94 @@ class Featurize(object):
                 warn('No method available to featurize "{}"'.format(column))
         return df
 
-    #TODO: -AF see if the only use of featurize_* methods is to be called in featurize_columns, think about defining them outside of the class
     def featurize_formula(self, df=None, featurizers='all', col_id="formula",
-                          compcol="composition", ignore_errors=True):
+                          compcol="composition", guess_oxidstate=True):
         if df is None:
             df = self.df.copy(deep=True)
         if compcol not in df:
             df[compcol] = df[col_id].apply(Composition)
+        if guess_oxidstate:
+            df[compcol] = composition_to_oxidcomposition(df[compcol])
         if featurizers=='all':
             featurizer = MultipleFeaturizer(self.all_featurizers.composition)
         else:
             featurizer = MultipleFeaturizer(featurizers)
         df = featurizer.featurize_dataframe(df,
                                             col_id=compcol,
-                                            ignore_errors=ignore_errors)
+                                            ignore_errors=self.ignore_errors)
         return df
 
 
-    def featurize_structure(self, df=None, col_id="structure", preset_name="ops"):
+    def featurize_structure(self, df=None, col_id="structure"):
         if df is None:
             df = self.df.copy(deep=True)
-        featurizer = MultipleFeaturizer([
-            sf.SiteStatsFingerprint(
-                site_featurizer=sf.CrystalSiteFingerprint.from_preset(
-                    preset=preset_name), stats=('mean', 'std_dev', 'minimum','maximum')
-            ),
-            sf.DensityFeatures(),
-            sf.GlobalSymmetryFeatures()
-        ])
-        df = featurizer.featurize_dataframe(df, col_id=col_id)
+
+        featurizer = MultipleFeaturizer(self.all_featurizers.structure)
+        df = featurizer.featurize_dataframe(df,
+                                            col_id=col_id,
+                                            ignore_errors=self.ignore_errors)
         return df
 
 
 class AllFeaturizers(object):
+    """
+    Contains all available featurizers that take various types of inputs such
+    as "composition" or "formula", "structure", "dos", "band structure", etc.
+    The point of this class is to have one place where available featurizers
+    with default inputs are updated synced with matminer.
 
+    Args:
+        preset_name (str): some featurizers take in this argument
+    """
     def __init__(self, preset_name="matminer"):
         self.preset_name = preset_name
 
     @property
     def composition(self, preset_name=None):
+        """
+        All composition-based featurizers with default arguments.
+
+        Args:
+            preset_name (str): some featurizers take in this argument
+
+        Returns ([matminer featurizer classes]):
+
+        """
         preset_name = preset_name or self.preset_name
         return [
-                cf.ElementProperty.from_preset(preset_name=preset_name),
-                cf.IonProperty()
-            ]
+            cf.ElementProperty.from_preset(preset_name=preset_name),
+            cf.CationProperty.from_preset(preset_name='deml'),
+            cf.IonProperty(),
+        ]
+
+    @property
+    def structure(self, preset_name="ops"):
+        """
+        All structure-based featurizers with default arguments.
+
+        Args:
+            preset_name (str): some featurizers take in this argument
+
+        Returns ([matminer featurizer classes]):
+
+        """
+        preset_name = preset_name or self.preset_name
+        return [
+            sf.SiteStatsFingerprint(
+                site_featurizer=sf.CrystalSiteFingerprint.from_preset(
+                    preset=preset_name),
+                stats=('mean', 'std_dev', 'minimum','maximum')),
+            sf.DensityFeatures(),
+            sf.GlobalSymmetryFeatures(),
+        ]
+
 
 
 if __name__ == "__main__":
     df_init = load_double_perovskites_gap(return_lumo=False)
     featurizer = Featurize(df_init,
-                     ignore_cols=['a_1', 'a_2', 'b_1', 'b_2'])
+                           ignore_cols=['a_1', 'a_2', 'b_1', 'b_2'],
+                           ignore_errors=False)
     df = featurizer.featurize_columns()
     # df = featurizer.featurize_formula(df_init, featurizers='all')
 
