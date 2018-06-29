e9520ed65cdf876934367afbd0b5b4b04f072d98
Alireza <alireza.faghaninia@gmail.com>
2018-06-29 10:21:29 -0700

    add and modify tests

--- matbench/tests/test_featurize.py
+++ matbench/tests/test_featurize.py
@@ -4,6 +4,7 @@ import unittest
 
 from matbench.data.load import load_double_perovskites_gap
 from matbench.featurize import Featurize
+from matminer.featurizers.composition import ElementProperty, IonProperty
 
 
 class TestFeaturize(unittest.TestCase):
@@ -14,17 +15,24 @@ class TestFeaturize(unittest.TestCase):
         featurizer = Featurize(df_init,
                                ignore_cols=ignore_cols,
                                ignore_errors=False)
-        df = featurizer.featurize_columns()
+        df = featurizer.featurize_formula()
         self.assertTrue("composition" in df)
         self.assertTrue(len(df), limit)
         self.assertGreaterEqual(len(df.columns), 90)
         self.assertTrue(featurizer.df.equals(df_init.drop(ignore_cols,axis=1)))
+        self.assertAlmostEqual(
+            df[df["formula"]=="AgNbSnTiO6"]["band center"].values[0],-2.623, 3)
 
-        # making sure featurize_formula works with only composition
+        # making sure:
+            # featurize_formula works with only composition and not formula
+            # eaturize_formula works with a given list of featurizers
         df_init = df_init.drop('formula', axis=1)
         df_init["composition"] = df["composition"]
-        df = featurizer.featurize_formula(df_init, featurizers='all')
-        self.assertGreaterEqual(len(df.columns), 90)
+        df = featurizer.featurize_formula(df_init, featurizers=[
+            ElementProperty.from_preset(preset_name="matminer"),
+            IonProperty()
+        ])
+        self.assertGreaterEqual(len(df.columns), 70)
 
 
 
