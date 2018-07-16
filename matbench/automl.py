272a7f507f46f2d2d0f03d38034b9ecd54ae9759
Alireza <alireza.faghaninia@gmail.com>
2018-07-16 11:02:52 -0700

    add automl and reasons why it might be a bad option for automation

---
+++ matbench/automl.py
@@ -0,0 +1,34 @@
+from auto_ml import Predictor
+from auto_ml.utils import get_boston_dataset
+
+"""
+Test of automl package as a candidate for main "auto machine learning" engine
+for matbench.
+
+-AF: here are some major advantages and disadvantages I found:
+    pros:
+        + easy to install
+        + easy to get it to work and print some results
+    cons:
+        - lack of documentation and organization
+        - results are printed or to ".dill" file. Difficult to handle the results
+    
+    Might not be the best option.
+"""
+
+
+df_train, df_test = get_boston_dataset()
+
+column_descriptions = {
+    'MEDV': 'output',
+    'CHAS': 'categorical'
+}
+
+ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
+# ml_predictor.set_params_and_defaults(df_train, compare_all_models=True)
+
+ml_predictor.train(df_train)
+score = ml_predictor.score(df_test, df_test.MEDV)
+
+print(score)
+# ml_predictor.save()
