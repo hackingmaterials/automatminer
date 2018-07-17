73e8296caad586daed97e876a3d49b1b419a1db8
Alireza <alireza.faghaninia@gmail.com>
2018-07-17 15:05:42 -0700

    add restrictions on available fields and formats of loader funcs docs

--- matbench/data/test_data.py
+++ matbench/data/test_data.py
@@ -10,15 +10,20 @@ import matminer.datasets.dataframe_loader as blacklist_source
 class MatbenchTestData(unittest.TestCase):
 
     def test_load_data(self):
-        loader_funcs = []
+        loader_func_names = []
         for f in dir(loaders_source):
             if 'load_' in f and f not in dir(blacklist_source):
-                loader_funcs.append(f)
-        print(loader_funcs)
-        for loader_func in loader_funcs:
-            print('testing "{}()" ...'.format(loader_func))
-            df = getattr(loaders_source, loader_func)()
+                loader_func_names.append(f)
+
+        print(loader_func_names)
+        for loader_func_name in loader_func_names:
+            print('testing "{}()" ...'.format(loader_func_name))
+            loader_func = getattr(loaders_source, loader_func_name)
+            df = loader_func()
+            loader_func_doc = loader_func.__doc__
             self.assertTrue(isinstance(df, pd.DataFrame))
+            for field in ['References:', 'Returns:', '(output):', '(input):']:
+                self.assertTrue(field in loader_func_doc)
             for col in df:
                 self.assertEqual(col, col.lower())
                 self.assertLessEqual(len(col), 20)
