b4cbf0e87423794ad29eaf0c77c4703048a3cb08
Alireza <alireza.faghaninia@gmail.com>
2018-07-19 15:00:51 -0700

    add top score tests with a class RS (random_state) variable for consistency

--- matbench/tests/test_tpot.py
+++ matbench/tests/test_tpot.py
@@ -9,7 +9,10 @@ from sklearn.model_selection import train_test_split
 
 class TestTpotAutoml(unittest.TestCase):
 
-    def test_tpot(self, limit=200):
+    def setUp(self):
+        self.RS = 27
+
+    def test_tpot(self, limit=500):
         target_col = 'gap gllbsc'
         # load and featurize:
         df_init = load_double_perovskites_gap(return_lumo=False)[:limit]
@@ -23,18 +26,30 @@ class TestTpotAutoml(unittest.TestCase):
         # train/test split (development is within tpot crossvalidation)
         X_train, X_test, y_train, y_test = \
             train_test_split(df.drop(target_col, axis=1).as_matrix(),
-            df[target_col], train_size=0.75, test_size=0.25)
+            df[target_col], train_size=0.75, test_size=0.25, random_state=self.RS)
 
         tpot = TpotAutoml(model_type='regressor',
                           generations=1,
                           population_size=25,
                           scoring='r2',
-                          random_state=23)
+                          random_state=self.RS)
         self.assertTrue(tpot.scoring_function=='r2')
         tpot.fit(X_train, y_train)
 
-        selected_models = tpot.get_selected_models()
+        top_scores = tpot.get_top_models(return_scores=True)
+        print(top_scores)
         self.assertTrue(tpot.greater_score_is_better)
+        self.assertAlmostEqual(top_scores['XGBRegressor'], 0.8622, 3)
+        self.assertAlmostEqual(top_scores['ExtraTreesRegressor'], 0.7933, 3)
+        self.assertAlmostEqual(top_scores['DecisionTreeRegressor'], 0.7709, 3)
+        self.assertAlmostEqual(top_scores['LassoLarsCV'], 0.7058, 3)
+        self.assertAlmostEqual(top_scores['RandomForestRegressor'], 0.7495, 3)
+        self.assertAlmostEqual(top_scores['GradientBoostingRegressor'], 0.7352, 3)
+        self.assertAlmostEqual(top_scores['ElasticNetCV'], 0.7124, 3)
+        self.assertAlmostEqual(top_scores['KNeighborsRegressor'], 0.4808, 2)
+        self.assertAlmostEqual(top_scores['LinearSVR'], 0.5, 1)
+        test_score = tpot.score(X_test, y_test)
+        self.assertAlmostEqual(test_score, 0.8707, places=3)
 
 
 if __name__ == '__main__':
