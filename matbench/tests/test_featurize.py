cd639661dea6048d5e9fc48b9c104d384e3e547b
Alireza <alireza.faghaninia@gmail.com>
2018-07-31 15:25:00 -0700

    got rid of fit_structure

--- matbench/tests/test_featurize.py
+++ matbench/tests/test_featurize.py
@@ -284,7 +284,7 @@ class TestAllFeaturizers(unittest.TestCase):
         # get all current featurizers
         true_feats = self.get_true_featurizers(sf, non_featurizers)
         # get all featurizers that are defined in AllFeaturizers class
-        test_feats = self.allfs.structure() + self.allfs.fit_structure()
+        test_feats = self.allfs.structure()
         test_feats = [c.__class__.__name__ for c in test_feats]
         # featurizers must match exactly
         self.assertEqual(len(test_feats), len(true_feats))
