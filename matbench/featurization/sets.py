f95c2a5ba6c5a6c6c801672f2bff52b07a751b7f
Alex Ganose <alexganose@googlemail.com>
2018-10-10 10:43:31 -0700

    Tidy FeaturizerSet and AllFeaturizers

--- matbench/featurization/sets.py
+++ matbench/featurization/sets.py
@@ -1,98 +1,120 @@
 """
 Defines sets of featurizers to be used by matbench during featurization.
 
-Classes of featurizer sets should have attributes being lists of featurizers.
+Featurizer sets are classes with attributes containing lists of featurizers.
 For example, the set of all fast structure featurizers could be found with:
-    StructureFeaturizers().fast
+
+    `StructureFeaturizers().fast`
 """
+
 import matminer.featurizers.composition as cf
 import matminer.featurizers.structure as sf
 import matminer.featurizers.dos as dosf
 import matminer.featurizers.bandstructure as bf
 
-__authors__ = ["Alex Dunn"]
+__authors__ = ["Alex Dunn", "Alex Ganose"]
 
 
 class FeaturizerSet:
-    """
-    An abstract class for defining sets of featurizers and the methods they
-    must implement.
+    """Abstract class for defining sets of featurizers.
+
+    All FeaturizerSets should implement at least two sets of featurizers, best
+    and all. The set of best featurizers should contain those featurizers
+    that balance speed, applicability and usefulness. This should be determined
+    by the implementor.
 
     Each set returned is a list of matminer featurizer objects.
 
     Args:
-        exclude ([str]): The class names of the featurizers which should be
-            excluded.
+        exclude (list of str, optional): A list of featurizer class names that
+            will be excluded from the set of featurizers returned.
     """
 
     def __init__(self, exclude=None):
-        self.exclude = [] if exclude is None else exclude
+        self.exclude = exclude if exclude else None
 
     def __call__(self, *args, **kwargs):
         return self.all
 
+    @property
     def best(self):
-        """
-        A set of featurizers that generally gives informative features without
-        excessive featurization time.
+        """List of featurizers providing useful features in a reasonable time.
+
+        Featurizers that take a very long time to run, which crash for many
+        systems, or which produce a large number of similar features will be
+        excluded.
         """
         raise NotImplementedError("This featurizer set must return a set of "
                                   "best featurizers")
 
+    @property
     def all(self):
-        """
-        All featurizers available in matminer for this featurization type.
-        """
+        """All featurizers available for this featurization type."""
         raise NotImplementedError("This featurizer set must return a set of "
                                   "all featurizers")
 
+    def _get_featurizers(self, featurizers):
+        """Utility function for getting featurizers not in the ignore list."""
+        return [f for f in featurizers
+                if f.__class__.__name__ not in self.exclude]
+
 
 class AllFeaturizers(FeaturizerSet):
-    """
-    Args:
-        exclude ([str]): The class names of the featurizers which should be
-            excluded.
+    """Featurizer set containing all available featurizers.
 
-    Example usage:
+    This class provides subsets for composition, structure, density of states
+    and band structure based featurizers.
+
+    Args:
+        exclude (list of str, optional): A list of featurizer class names that
+            will be excluded from the set of featurizers returned.
+
+    Example usage::
+
         composition_featurizers = AllFeaturizers().composition
     """
 
+    def __init__(self, exclude=None):
+        super(FeaturizerSet, self).__init__(exclude=exclude)
+
+        self._featurizer_sets = {
+            'comp': CompositionFeaturizers(),
+            'struct': StructureFeaturizers(),
+            'bs': BSFeaturizers(),
+            'dos': DOSFeaturizers()
+        }
+
     @property
     def best(self):
-        featzers = CompositionFeaturizers().best + \
-                   StructureFeaturizers().best + \
-                   BSFeaturizers().best + \
-                   DOSFeaturizers().best
-
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """See base class."""
+        featurizers = [f.best for f in self._featurizer_sets.values()]
+        return self._get_featurizers(featurizers)
 
     @property
     def all(self):
-        featzers = CompositionFeaturizers().all + \
-                   StructureFeaturizers().all + \
-                   BSFeaturizers().all + \
-                   DOSFeaturizers().all
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """See base class."""
+        featurizers = [f.all for f in self._featurizer_sets.values()]
+        return self._get_featurizers(featurizers)
 
     @property
     def composition(self):
-        featzers = CompositionFeaturizers().all
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """List of all composition based featurizers."""
+        return self._get_featurizers(self._featurizer_sets['comp'].all)
 
     @property
     def structure(self):
-        featzers = StructureFeaturizers().all
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """List of all structure based featurizers."""
+        return self._get_featurizers(self._featurizer_sets['struct'].all)
 
     @property
     def bandstructure(self):
-        featzers = BSFeaturizers().all
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """List of all band structure based featurizers."""
+        return self._get_featurizers(self._featurizer_sets['bs'].all)
 
     @property
     def dos(self):
-        featzers = DOSFeaturizers().all
-        return [i for i in featzers if i.__class__.__name__ not in self.exclude]
+        """List of all density of states based featurizers."""
+        return self._get_featurizers(self._featurizer_sets['dos'].all)
 
 
 class CompositionFeaturizers(FeaturizerSet):
