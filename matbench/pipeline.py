8946efe562b2082910d4ffc5319987096494ead3
ardunn <ardunn@lbl.gov>
2018-10-14 19:18:44 -0700

    further improvements to pipeline

--- matbench/pipeline.py
+++ matbench/pipeline.py
@@ -18,28 +18,19 @@ class PredictionPipeline:
     Also contains all data needed for generating reports and understanding
     a matbench pipeline that was executed.
     """
-    def __init__(self, df, target_key, time_limit=None,
-                 test_frac=0.1, aux_keys=None, logpath=".",
-                 loglvl=logging.INFO, persistence_lvl=2, name=None):
-        self.train_df = prescreen_df(df)
-        self.target_key = target_key
+    def __init__(self, time_limit=None, persistence_lvl=2, name=None):
         self.time_limit = time_limit
-        self.test_frac = test_frac
-        self.aux_keys = aux_keys
         self.persistence_lvl = persistence_lvl
-        self.logger = setup_custom_logger(filepath=logpath, level=loglvl)
         self.pipetype = None
         self.name = name
 
         if self.persistence_lvl > 1:
             now = datetime.datetime.now().isoformat()
             input_df_name = "input_df_{}.json".format(now)
-            self.logger.log("Saving input df as {}".format(input_df_name))
-            self.train_df.to_json()
 
-        self.best_model
+        self.best_model = None
 
-    def fit(self):
+    def fit(self, df, target_key, test_frac=None):
         """
         Benchmarks an AutoML pipeline including featurization, feature
         reduction, and model selection.
@@ -47,19 +38,19 @@ class PredictionPipeline:
         Returns:
             A MatbenchPipeline object
         """
-        self.logger.log("Beginning benchmarking.\n")
         self.pipetype = "benchmark"
 
         # TODO: featurizer selection should go here
+        #self.featurizers = FeaturizerSelection()
 
         f = Featurization()
-        df = f.auto_featurize(self.train_df)
+        df = f.auto_featurize(df)
 
         if self.persistence_lvl > 1:
             df.to_json("{}_featurized_df_{}.json".format(self.name, datetime.datetime.now().isoformat()))
 
         p = Preprocessing()
-        df = p.preprocess(df, "e_form", scale=True, max_na_frac=0.01)
+        df = p.preprocess(df, target_key, scale=True, max_na_frac=0.01)
 
         if self.persistence_lvl > 1:
             df.to_json("{}_preprocessed_df_{}.json".format(self.name, datetime.datetime.now().isoformat()))
@@ -69,24 +60,25 @@ class PredictionPipeline:
         tpot = TpotAutoml(mode="regression",
                           max_time_mins=360,
                           scoring='neg_mean_absolute_error',
-                          feature_names=df.drop(self.target_key, axis=1).columns,
+                          feature_names=df.drop(target_key, axis=1).columns,
                           n_jobs=8,
                           verbosity=2)
 
         X_train, X_test, y_train, y_test = train_test_split(
-            df.drop(self.target_key, axis=1).values, df[self.target_key],
-            test_size=self.test_frac)
+            df.drop(target_key, axis=1).values, df[target_key],
+            test_size=test_frac)
 
         tpot.fit(X_train, y_train)
-        validation_score = tpot.score(X_test, y_test)
+        self.validation_score = tpot.score(X_test, y_test)
+        self.top_models = tpot.get_top_models(return_scores=True)
 
         # todo: should save tpot model here
-        return {"top_model_scores": tpot.get_top_models(return_scores=True),
-                "best_model_validation_score": validation_score}
+        # self.tpot_top_predictor = ??
+        return self
 
     def predict(self, predict_df):
         """
-        Using the data in df, makes predictions with an automated pipeline.
+        Using pipeline created with fit, makes predictions with an automated pipeline.
 
         Args:
             predict_df: The dataframe containing
