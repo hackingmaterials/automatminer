ccb68496a9febb8f411c0977572ae673bcc49020
ardunn <ardunn@lbl.gov>
2018-10-26 13:28:12 -0700

    add dataframetransformer and mv custom error class 2 base file

---
+++ matbench/base.py
@@ -0,0 +1,30 @@
+from sklearn.base import TransformerMixin
+
+class MatbenchError(BaseException):
+    """
+    Exception specific to matbench methods.
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return "MatbenchError : " + self.msg
+
+class DataFrameTransformer:
+    """
+    Abstract base class for transforming dataframes in the same way BaseEstimator
+    and Tra
+    """
+
+    def fit(self, df, target, *args, **kwargs):
+        raise NotImplementedError(
+            "All dataframe transformers must implement fit.")
+
+    def transform(self, df, target, *args, **kwargs):
+        raise NotImplementedError(
+            "All dataframe transformers must implement transform.")
+
+    def fit_transform(self, df, target, *args, **kwargs):
+        self.fit(df, target, *args, **kwargs)
+        return self.transform(df, target, )
