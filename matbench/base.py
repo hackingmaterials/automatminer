b30d0ba18f707e83e9d11924f8e0e26342b802ff
ardunn <ardunn@lbl.gov>
2018-10-26 14:01:57 -0700

    rm dataframetransformer in favor of transformermixin + baseestimator

--- matbench/base.py
+++ matbench/base.py
@@ -10,21 +10,3 @@ class MatbenchError(BaseException):
         return "MatbenchError : " + self.msg
 
 
-class DataFrameTransformer:
-    """
-    Abstract base class for transforming dataframes in the same way
-    BaseEstimator and TransformerMixin are abc's for sklearn matrix
-    transformation.
-    """
-
-    def fit(self, df, target):
-        raise NotImplementedError(
-            "All dataframe transformers must implement fit.")
-
-    def transform(self, df, target):
-        raise NotImplementedError(
-            "All dataframe transformers must implement transform.")
-
-    def fit_transform(self, df, target):
-        self.fit(df, target)
-        return self.transform(df, target, )
