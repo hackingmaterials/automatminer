750df3a65c43450b1669792bd077153806c7957e
ardunn <ardunn@lbl.gov>
2018-10-29 15:32:33 -0700

    Add abc for automl, fix a typo in dftransformer

--- matbench/base.py
+++ matbench/base.py
@@ -34,16 +34,35 @@ class LoggableMixin:
 
 class DataframeTransformer:
     """
-    A class to allow easy transformation in the same way as TransformerMixin
-    and BaseEstimator in sklearn.
+    A base class to allow easy transformation in the same way as
+    TransformerMixin and BaseEstimator in sklearn.
     """
     def fit(self, df, target):
         raise NotImplementedError("{} has no fit method implemented!".format(self.__class__.__name__))
 
-    def transformer(self, df, target):
+    def transform(self, df, target):
         raise NotImplementedError("{} has no transform method implemented!".format(self.__class__.__name__))
 
     def fit_transform(self, df, target):
         return self.fit(df, target).transform(df, target)
 
 
+class AutoMLAdaptor:
+    """
+    A base class to adapt from an AutoML backend to a sklearn-style fit/predict
+    scheme and add a few extensions.
+    """
+
+    def fit(self, df, target):
+        raise NotImplementedError("{} has no fit method implemented!".format(self.__class__.__name__))
+
+    def predict(self, df):
+        raise NotImplementedError("{} has no predict method implemented!".format(self.__class__.__name__))
+
+    @property
+    def features(self):
+        raise NotImplementedError("{} has no features attr implemented!".format(self.__class__.__name__))
+
+    @property
+    def best_model(self):
+        raise NotImplementedError("{} has no best model attr implemented!".format(self.__class__.__name__))
