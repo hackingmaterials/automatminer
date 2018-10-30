ced4f86facdd84b2446a797b74f260db829fee45
ardunn <ardunn@lbl.gov>
2018-10-29 17:11:25 -0700

    updated docs

--- matbench/base.py
+++ matbench/base.py
@@ -38,31 +38,113 @@ class DataframeTransformer:
     TransformerMixin and BaseEstimator in sklearn.
     """
     def fit(self, df, target):
+        """
+        Fits the transformer to a dataframe, given a target.
+
+        Args:
+            df (pandas.DataFrame): The pandas dataframe to be fit.
+            target (str): the target string specifying the ML target.
+
+        Returns:
+            (AutoMLAdaptor) This object (self)
+
+        """
         raise NotImplementedError("{} has no fit method implemented!".format(self.__class__.__name__))
 
     def transform(self, df, target):
+        """
+        Transforms a dataframe.
+
+        Args:
+            df (pandas.DataFrame): The pandas dataframe to be fit.
+            target (str): the target string specifying the ML target.
+
+        Returns:
+            (pandas.DataFrame): The transformed dataframe.
+
+        """
         raise NotImplementedError("{} has no transform method implemented!".format(self.__class__.__name__))
 
     def fit_transform(self, df, target):
+        """
+        Combines the fitting and transformation of a dataframe.
+
+        Args:
+            df (pandas.DataFrame): The pandas dataframe to be fit.
+            target (str): the target string specifying the ML target.
+
+        Returns:
+            (pandas.DataFrame): The transformed dataframe.
+
+        """
         return self.fit(df, target).transform(df, target)
 
 
-class AutoMLAdaptor:
+class AutoMLAdaptor(DataframeTransformer):
     """
     A base class to adapt from an AutoML backend to a sklearn-style fit/predict
     scheme and add a few extensions.
     """
 
-    def fit(self, df, target):
-        raise NotImplementedError("{} has no fit method implemented!".format(self.__class__.__name__))
+    def predict(self, df, target):
+        """
+        Using a fitted object, use the best model available to transform a
+        dataframe not containing the target to a dataframe containing the
+        predicted target values.
 
-    def predict(self, df):
+        Analagous to DataframeTransformer.transform
+
+        Args:
+            df (pandas.DataFrame): The dataframe to-be-predicted
+            target: The target metric to be predicted. The output column for
+                the data will be "predicted {target}".
+
+        Returns:
+            (pandas.DataFrame): The dataframe updated with predictions of the
+                target property.
+
+        """
         raise NotImplementedError("{} has no predict method implemented!".format(self.__class__.__name__))
 
     @property
     def features(self):
+        """
+        The features being used for machine learning.
+
+        Returns:
+            ([str]): The feature labels
+        """
         raise NotImplementedError("{} has no features attr implemented!".format(self.__class__.__name__))
 
     @property
-    def best_model(self):
-        raise NotImplementedError("{} has no best model attr implemented!".format(self.__class__.__name__))
+    def ml_data(self):
+        """
+        The raw ML-data being passed to the backend.
+
+        Returns:
+            (dict): At minimum, the raw X and y matrices being used for training.
+                May also contain other data.
+        """
+        raise NotImplementedError("{} has no ML data attr implemented!".format(self.__class__.__name__))
+
+
+    @property
+    def best_models(self):
+        """
+        The best models returned by the AutoML backend.
+
+        Returns:
+            (list or OrderedDict}: The best models as determined by the AutoML package.
+        """
+        raise NotImplementedError("{} has no best models attr implemented!".format(self.__class__.__name__))
+
+    @property
+    def backend(self):
+        """
+        The raw, fitted backend object, if it exists.
+
+        Returns:
+            Backend object (e.g., TPOTClassifier)
+
+        """
+        raise NotImplementedError("{} has no backend object attr implemented!".format(self.__class__.__name__))
