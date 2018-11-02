407163ad8fdbe5def97ef08b21b5aa324c5fd5df
Qi Wang <wqthu11@gmail.com>
2018-11-02 04:18:06 -0700

    including tests for metafeature + exclude etc in featurization.tests

--- matbench/featurization/tests/test_core.py
+++ matbench/featurization/tests/test_core.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import os
+import copy
 import unittest
 
 import pandas as pd
@@ -16,8 +17,8 @@ from matbench.featurization.core import AutoFeaturizer
 test_dir = os.path.dirname(__file__)
 
 __author__ = ["Alex Dunn <ardunn@lbl.gov>",
-              "Alireza Faghaninia <alireza@lbl.gov>"]
-
+              "Alireza Faghaninia <alireza@lbl.gov>",
+              "Qi Wang <wqthu11@gmail.com>"]
 
 
 class TestAutoFeaturizer(unittest.TestCase):
@@ -95,7 +96,7 @@ class TestAutoFeaturizer(unittest.TestCase):
         """
         Test custom args for featurizers to use.
         """
-        df = self.test_df[:self.limit]
+        df = copy.copy(self.test_df.iloc[:self.limit])
         target = "K_VRH"
         exclude = ["ElementProperty"]
 
@@ -111,21 +112,58 @@ class TestAutoFeaturizer(unittest.TestCase):
         # Test to make sure excluded does not show up
         af = AutoFeaturizer(exclude=exclude, use_metaselector=False)
         af.fit(df, target)
-        print(af.features)
         df = af.fit_transform(df, target)
         self.assertFalse(any([f in df.columns for f in ep_feats]))
 
         # Test to see if metaselector works for this dataset
+        df = copy.copy(self.test_df.iloc[:self.limit])
         af = AutoFeaturizer(exclude=exclude, use_metaselector=True)
         af.fit(df, target)
-        print(af.features)
+        dataset_mfs = af.metaselector.dataset_mfs
+        self.assertIn("composition_metafeatures", dataset_mfs.keys())
+        self.assertIn("structure_metafeatures", dataset_mfs.keys())
+        self.assertIsNotNone(dataset_mfs["composition_metafeatures"])
+        self.assertIsNotNone(dataset_mfs["structure_metafeatures"])
+
+        comp_mfs = dataset_mfs["composition_metafeatures"]
+        self.assertEqual(comp_mfs["number_of_compositions"], 5)
+        self.assertAlmostEqual(comp_mfs["percent_of_all_metal"], 0.2)
+        self.assertAlmostEqual(
+            comp_mfs["percent_of_metal_nonmetal"], 0.8)
+        self.assertAlmostEqual(comp_mfs["percent_of_all_nonmetal"], 0.0)
+        self.assertAlmostEqual(
+            comp_mfs["percent_of_contain_trans_metal"], 0.8)
+        self.assertEqual(comp_mfs["number_of_different_elements"], 7)
+        self.assertAlmostEqual(comp_mfs["avg_number_of_elements"], 2.2)
+        self.assertEqual(comp_mfs["max_number_of_elements"], 3)
+        self.assertEqual(comp_mfs["min_number_of_elements"], 1)
+
+        struct_mfs = dataset_mfs["structure_metafeatures"]
+        self.assertEqual(struct_mfs["number_of_structures"], 5)
+        self.assertAlmostEqual(struct_mfs["percent_of_ordered_structures"], 1.0)
+        self.assertAlmostEqual(struct_mfs["avg_number_of_sites"], 7.0)
+        self.assertEqual(struct_mfs["max_number_of_sites"], 12)
+        self.assertEqual(
+            struct_mfs["number_of_different_elements_in_structures"], 7)
+
+        excludes = af.metaselector.excludes
+        self.assertIn("ElementProperty", excludes)
+        self.assertIn("IonProperty", excludes)
+        self.assertIn("Miedema", excludes)
+        self.assertIn("OxidationStates", excludes)
+        self.assertIn("YangSolidSolution", excludes)
+        self.assertIn("TMetalFraction", excludes)
+        self.assertIn("ElectronegativityDiff", excludes)
+        self.assertIn("CationProperty", excludes)
+        self.assertIn("ElectronAffinity", excludes)
+
         df = af.fit_transform(df, target)
         self.assertFalse(any([f in df.columns for f in ep_feats]))
 
         # Test to make sure composition features are not automatically
         # created if explicitly not included in featurizers dict
         # Also make sure no errors when excluding non-present featurizers
-        df = self.test_df[:self.limit]
+        df = copy.copy(self.test_df.iloc[:self.limit])
         featurizers = {"structure": [gsf, dn]}
         af = AutoFeaturizer(featurizers=featurizers)
         df = af.fit_transform(df, target)
@@ -202,7 +240,6 @@ class TestAutoFeaturizer(unittest.TestCase):
         af.fit(df1, target)
 
         df2 = af.transform(df2, target)
-        print(df2)
         self.assertAlmostEqual(df2[target].iloc[0], 111.788114, places=5)
         self.assertAlmostEqual(df2["minimum X"].iloc[1], 1.36, places=2)
 
