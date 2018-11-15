a73d3b7a93d3cf9c6407dbe6a8a263a808e13822
ardunn <ardunn@lbl.gov>
2018-11-14 21:30:05 -0800

    final update to tpot tests

--- mslearn/automl/tests/test_tpot.py
+++ mslearn/automl/tests/test_tpot.py
@@ -15,10 +15,9 @@ __author__ = ['Alex Dunn <ardunn@lbl.gov']
 class TestTPOTAdaptor(unittest.TestCase):
     def setUp(self):
         basedir = os.path.dirname(os.path.realpath(__file__))
-        self.train_df = pd.read_csv(basedir + "/mini_training_df_automl.csv",
-                                    index_col=0)
-        self.test_df = pd.read_csv(basedir + "/mini_test_df_automl.csv",
-                                   index_col=0)
+        df = pd.read_csv(basedir + "/mini_automl_df.csv", index_col=0)
+        self.train_df = df.copy(deep=True).iloc[:450]
+        self.test_df = df.copy(deep=True).iloc[451:]
         self.common_tpot_kwargs = {"max_time_mins": 5, "max_eval_time_mins": 5,
                                    "generations": 10, "population_size": 15}
 
@@ -36,9 +35,11 @@ class TestTPOTAdaptor(unittest.TestCase):
         max_kvrh = 50
         classifier_key = "K_VRH > {}?".format(max_kvrh)
         train_df = self.train_df.rename(columns={"K_VRH": classifier_key})
+        test_df = self.test_df.rename(columns={"K_VRH": classifier_key})
         train_df[classifier_key] = train_df[classifier_key] > max_kvrh
+        test_df[classifier_key] = test_df[classifier_key] > max_kvrh
         tpot.fit(train_df, classifier_key)
-        test_w_predictions = tpot.predict(self.test_df, classifier_key)
+        test_w_predictions = tpot.predict(test_df, classifier_key)
         y_true = test_w_predictions[classifier_key]
         y_test = test_w_predictions[classifier_key + " predicted"]
         self.assertTrue(f1_score(y_true, y_test) > 0.75)
@@ -49,13 +50,13 @@ class TestTPOTAdaptor(unittest.TestCase):
         train_w_predictions = tpot.fit_transform(self.train_df, target_key)
         y_true = train_w_predictions[target_key]
         y_test = train_w_predictions[target_key + " predicted"]
-        self.assertTrue(f1_score(y_true, y_test) > 0.85)
+        self.assertTrue(r2_score(y_true, y_test) > 0.85)
 
     def test_feature_mismatching(self):
         tpot = TPOTAdaptor(mode="regression", **self.common_tpot_kwargs)
         target_key = "K_VRH"
-        df1 = self.test_df
-        df2 = self.train_df.rename(columns={'mean X': "some other feature"})
+        df1 = self.train_df
+        df2 = self.test_df.rename(columns={'mean X': "some other feature"})
         tpot.fit(df1, target_key)
         with self.assertRaises(MatbenchError):
             tpot.predict(df2, target_key)
