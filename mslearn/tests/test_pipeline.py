021d67a71dcaf0982032463fc6196ddcee62b198
ardunn <ardunn@lbl.gov>
2018-11-15 11:43:39 -0800

    skeleton out pipeline tests

---
+++ mslearn/tests/test_pipeline.py
@@ -0,0 +1,19 @@
+import unittest
+
+from mslearn.pipeline import MatPipe
+
+class TestMatPipe(unittest.TestCase):
+    def setUp(self):
+        self.pipe1 = MatPipe()
+
+    def test_transferability(self):
+        self.assertTrue(True)
+
+    def test_benchmarking(self):
+        self.assertTrue(True)
+
+    def test_persistence(self):
+        self.assertTrue(True)
+
+    def tesT_digest(self):
+        self.assertTrue(True)
