235be4f5a5981ddc6119f737d94e3598bc5fc0e4
Daniel Dopp <dbdopp@lbl.gov>
2018-11-18 13:53:01 -0800

    Adding partial dependency plotting part 2

--- mslearn/analytics/core.py
+++ mslearn/analytics/core.py
@@ -1,3 +1,6 @@
+import os
+
+import matplotlib.pyplot as plt
 from skater.core.explanations import Interpretation
 from skater.model import InMemoryModel
 from matminer.datasets import load_dataset
@@ -21,9 +24,12 @@ class Analytics:
         self.interpreter = Interpretation(self.dataset,
                                           feature_names=self.feature_labels)
 
+        def predict_func(x):
+            prediction = self.predictive_model.learner.predict(x, self.target)
+            return prediction[self.target + " predicted"].values
+
         self.model = InMemoryModel(
-            self.predictive_model.learner._backend.predict,
-            examples=self.dataset
+            predict_func, examples=self.dataset
         )
 
     def get_feature_importance(self):
@@ -32,19 +38,38 @@ class Analytics:
         )
 
     def plot_partial_dependence(self, feature_ids):
-        return self.interpreter.partial_dependence.plot_partial_dependence(
-            feature_ids, self.model,
+        if not isinstance(feature_ids, list):
+            feature_ids = [feature_ids]
+
+        if len(feature_ids) != 1:
+            raise ValueError("Error, this method does not yet support "
+                             "computing plots for more than one feature at a "
+                             "time")
+
+        axs = self.interpreter.partial_dependence.plot_partial_dependence(
+            feature_ids, self.model, sample=False, progressbar=False,
+            with_variance=True
         )
+        axs[0][0].suptitle("Model dependance on {}".format(feature_ids[0]))
+        axs[0][1].set_ylabel("Average predicited {}".format(self.target))
+        plt.ticklabel_format(style='plain', axis='y', scilimits=(0, 0))
+        plt.savefig(feature_ids[0] + "_pdp.png")
 
 
 if __name__ == '__main__':
-    df = load_dataset('elastic_tensor_2015')
-    df = df[["formula", "K_VRH"]]
-    df = df.rename({"formula": "composition"}, axis=1)
+    if not os.path.exists("tests/test_pipe.p"):
+        df = load_dataset('elastic_tensor_2015')
+        df = df[["formula", "K_VRH"]]
+        df = df.rename({"formula": "composition"}, axis=1)
 
-    fitted_pipeline = MatPipe().fit(df, "K_VRH")
-    print("Done fitting")
+        fitted_pipeline = MatPipe().fit(df, "K_VRH")
+        fitted_pipeline.save("tests/test_pipe.p")
+    else:
+        fitted_pipeline = MatPipe().load("tests/test_pipe.p")
 
     analyzer = Analytics(fitted_pipeline)
 
-    print(analyzer.get_feature_importance())
+    feature_importance = analyzer.get_feature_importance()
+
+    for feature in reversed(feature_importance.index):
+        analyzer.plot_partial_dependence(feature)
