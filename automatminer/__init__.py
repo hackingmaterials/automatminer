0fd718aeeb881c4f0cd80e4b708856cf6227891e
ardunn <ardunn@lbl.gov>
2018-12-20 03:52:22 -0800

    update init and add tasks bsaic

--- automatminer/__init__.py
+++ automatminer/__init__.py
@@ -1,1 +1,10 @@
-from .preprocessing import DataCleaner, FeatureReducer
+from automatminer.preprocessing import DataCleaner, FeatureReducer
+from automatminer.automl import TPOTAdaptor
+from automatminer.pipeline import MatPipe
+from automatminer.configs import debug_config, default_config, \
+    production_config, fast_config
+
+__author__ = 'Alex Dunn, Qi Wang, Alex Ganose, Daniel Dopp, Anubhav Jain'
+__author_email__ = 'ardunn@lbl.gov'
+__license__ = 'Modified BSD'
+__version__ = '2018.12.11_beta'
