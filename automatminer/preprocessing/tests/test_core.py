676e17a35401b7566465decbfef217d5ba4f6cc2
ardunn <ardunn@lbl.gov>
2018-12-20 00:01:52 -0800

    update tests for new correlation and preprocessing

--- automatminer/preprocessing/tests/test_core.py
+++ automatminer/preprocessing/tests/test_core.py
@@ -1,23 +1,33 @@
 import os
 import unittest
+from copy import deepcopy
 
 import numpy as np
 import pandas as pd
 
 from automatminer.preprocessing.core import DataCleaner, FeatureReducer
-from automatminer.preprocessing.feature_selection import TreeBasedFeatureReduction, \
-    rebate
+from automatminer.preprocessing.feature_selection import TreeFeatureReducer, \
+    rebate, lower_corr_clf
 from automatminer.utils.package_tools import compare_columns
 
 test_dir = os.path.dirname(__file__)
 
 
 class TestPreprocess(unittest.TestCase):
-    # todo: need test case for categorical target and categorical features -AD
-
     def setUp(self):
         df = pd.read_csv(os.path.join(test_dir, 'test_featurized_df.csv'))
-        self.test_df = df.drop(columns=["formula"])
+        self._test_df = df.drop(columns=["formula"])
+        self.target = 'gap expt'
+
+    @property
+    def test_df(self):
+        """
+        Prevent any memory problems or accidental overwrites.
+
+        Returns:
+            (pd.DataFrame): A pandas deataframe deepcopy of the testing df.
+        """
+        return deepcopy(self._test_df)
 
     def test_DataCleaner(self):
         """
@@ -27,72 +37,79 @@ class TestPreprocess(unittest.TestCase):
         Returns: None
         """
         df = self.test_df
-        target = 'gap expt'
         dc = DataCleaner()
 
         # Test the case of numbers as strings
-        df[target] = df[target].astype(str)
-        df = dc.fit_transform(df, target)
-        self.assertAlmostEqual(df[target].iloc[0], 0.35)
+        df[self.target] = df[self.target].astype(str)
+        df = dc.fit_transform(df, self.target)
+        self.assertAlmostEqual(df[self.target].iloc[0], 0.35)
 
         # Test if there is an nan in target
-        df[target].iloc[8] = np.nan
-        df = dc.fit_transform(df, target)
+        df[self.target].iloc[8] = np.nan
+        df = dc.fit_transform(df, self.target)
         self.assertEqual(df.shape[0], self.test_df.shape[0] - 1)
 
         # Test if there is an nan in feature
         df['HOMO_energy'].iloc[40] = np.nan
-        df = dc.fit_transform(df, target)
+        df = dc.fit_transform(df, self.target)
         self.assertEqual(df.shape[0], self.test_df.shape[0] - 2)
 
         # Test if nan threshold is exceeded for a feature
         df["LUMO_energy"].iloc[:-2] = [np.nan] * (df.shape[0] - 2)
-        df = dc.fit_transform(df, target)
+        df = dc.fit_transform(df, self.target)
         self.assertEqual(df.shape[1], self.test_df.shape[1] - 1)
 
         # test transferability
         df2 = self.test_df
-        df2 = df2.drop(columns=[target])
-        df2 = dc.transform(df2, target)
-        self.assertFalse(compare_columns(df, df2, ignore=target)["mismatch"])
-        self.assertTrue(target not in df2.columns)
+        df2 = df2.drop(columns=[self.target])
+        df2 = dc.transform(df2, self.target)
+        self.assertFalse(compare_columns(df, df2,
+                                         ignore=self.target)["mismatch"])
+        self.assertTrue(self.target not in df2.columns)
 
-    def test_FeatureReducer(self):
-        df = self.test_df
-        target = 'gap expt'
+    def test_FeatureReducer_basic(self):
         fr = FeatureReducer()
 
         # ultra-basic case: are we reducing at least 1 feature?
-        df = fr.fit_transform(df, target)
+        df = fr.fit_transform(self.test_df, self.target)
         self.assertTrue(df.shape[1] < self.test_df.shape[1])
 
         # ensure metadata is being written correctly
-        self.assertTrue(target not in fr.retained_features)
+        self.assertTrue(self.target not in fr.retained_features)
         self.assertTrue(len(list(fr.removed_features.keys())) == 2)
 
+    def test_FeatureReducer_advanced(self):
         # ensure other combinations of feature reducers are working
         fr = FeatureReducer(reducers=('corr', 'rebate'), n_rebate_features=40)
-        df = fr.fit_transform(self.test_df, target)
-        self.assertEqual(df.shape[1], 41)  # 40 features + target
-        self.assertTrue(target in df.columns)
+        df = fr.fit_transform(self.test_df, self.target)
+        self.assertEqual(df.shape[1], 41)  # 40 features + self.target
+        self.assertTrue(self.target in df.columns)
 
+    def test_FeatureReducer_transferability(self):
         # ensure the same thing works when fraction is used
         fr = FeatureReducer(reducers=('rebate',), n_rebate_features=0.2)
-        df = fr.fit_transform(self.test_df, target)
+        df = fr.fit_transform(self.test_df, self.target)
         self.assertEqual(df.shape[1], 83 + 1)
 
         # test transferability
-        df2 = self.test_df
-        df2 = fr.transform(df2, target)
+        df2 = deepcopy(self.test_df)
+        df2 = fr.transform(df2, self.target)
         self.assertListEqual(df.columns.tolist(), df2.columns.tolist())
 
+    def test_FeatureReducer_classification(self):
         # test classification with corr matrix (no errors)
+        fr = FeatureReducer(reducers=('corr',))
         df_class = self.test_df
-        df_class[target] = ["semiconductor" if 0.0 < g < 3.0 else "nonmetal"
-                            for g in df_class[target]]
-        df_class = fr.fit_transform(df_class, target)
+        df_class[self.target] = ["semiconductor" if 0.0 < g < 3.0 else
+                                 "nonmetal" for g in df_class[self.target]]
+        df_class = fr.fit_transform(df_class, self.target)
         self.assertEqual(df_class.shape[0], 200)
-        self.assertEqual(df_class.shape[1], 84)
+
+    def test_FeatureReducer_combinations(self):
+        df = self.test_df
+        fr = FeatureReducer(reducers=('pca', 'rebate', 'tree'))
+        df_reduced = fr.fit_transform(df, self.target)
+        self.assertEqual(df_reduced.shape[1], 15)
 
 
 class TestFeatureReduction(unittest.TestCase):
@@ -105,8 +122,8 @@ class TestFeatureReduction(unittest.TestCase):
     def test_TreeBasedFeatureReduction(self):
         X = self.test_df.drop('gap expt', axis=1)
         y = self.test_df['gap expt']
-        tbfr = TreeBasedFeatureReduction(mode='regression',
-                                         random_state=self.random_state)
+        tbfr = TreeFeatureReducer(mode='regression',
+                                  random_state=self.random_state)
         tbfr.fit(X, y, cv=3)
         self.assertEqual(len(tbfr.selected_features), 19)
         X_reduced = tbfr.transform(X)
@@ -116,3 +133,8 @@ class TestFeatureReduction(unittest.TestCase):
     def test_rebate(self):
         df_reduced = rebate(self.test_df, "gap expt", 10)
         self.assertEqual(df_reduced.shape[1], 10)
+
+    def test_lower_corr_clf(self):
+        df = self.test_df
+        worse_feature = lower_corr_clf(df, "gap expt", "maximum X", "range row")
+        self.assertEqual("range row", worse_feature)
