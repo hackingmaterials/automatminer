785ce27397dd11fcf5a6a1a7aadd78390bebc15e
ardunn <ardunn@lbl.gov>
2019-01-25 21:48:50 -0800

    update presets

--- automatminer/presets.py
+++ automatminer/presets.py
@@ -7,44 +7,80 @@ Use them like so:
     pipe = MatPipe(**config)
 """
 
-__author__ = ["Alex Dunn <ardunn@lbl.gov>",
-              "Abhinav Ashar <AbhinavAshar@lbl.gov"]
+__author__ = ["Alex Dunn <ardunn@lbl.gov>"]
+
+from sklearn.ensemble import RandomForestRegressor
 
 from automatminer.featurization import AutoFeaturizer
 from automatminer.preprocessing import FeatureReducer, DataCleaner
-from automatminer.automl import TPOTAdaptor
+from automatminer.automl import TPOTAdaptor, SinglePipelineAdaptor
 
 
 def get_preset_config(preset='default'):
-    production_config = {"learner": TPOTAdaptor(generations=500,
-                                                population_size=500,
-                                                max_time_mins=720,
-                                                max_eval_time_mins=60),
-                         "reducer": FeatureReducer(),
-                         "autofeaturizer": AutoFeaturizer(preset="best"),
-                         "cleaner": DataCleaner()
-                         }
-    default_config = {"learner": TPOTAdaptor(max_time_mins=120),
-                      "reducer": FeatureReducer(),
-                      "autofeaturizer": AutoFeaturizer(preset="best"),
-                      "cleaner": DataCleaner()}
+    """
+    Returns preset configs for MatPipe.
 
-    fast_config = {"learner": TPOTAdaptor(max_time_mins=30, population_size=50),
-                   "reducer": FeatureReducer(reducers=('corr', 'tree')),
-                   "autofeaturizer": AutoFeaturizer(preset="fast"),
-                   "cleaner": DataCleaner()}
+    "default" - Good for typical usage
+    "production" - When high accuracy is required, and you have access to
+        powerful computing resources
+    "fast" - Good for quick benchmarks
+    "debug" - Debugging with automl enabled
+    "debug_single" - Debugging with a single model in an adaptor.
 
-    debug_config = {"learner": TPOTAdaptor(max_time_mins=2,
-                                           max_eval_time_mins=1,
-                                           population_size=10),
-                    "reducer": FeatureReducer(reducers=('corr',)),
-                    "autofeaturizer": AutoFeaturizer(preset="fast"),
-                    "cleaner": DataCleaner()}
+    Args:
+        preset (str): The name of the preset config you'd like to use.
+
+    Returns:
+        (dict) The desired preset config.
+
+    """
+    production_config = {
+        "learner": TPOTAdaptor(generations=500,
+                               population_size=500,
+                               max_time_mins=720,
+                               max_eval_time_mins=60),
+        "reducer": FeatureReducer(),
+        "autofeaturizer": AutoFeaturizer(preset="best"),
+        "cleaner": DataCleaner()
+    }
+
+    default_config = {
+        "learner": TPOTAdaptor(max_time_mins=120),
+        "reducer": FeatureReducer(),
+        "autofeaturizer": AutoFeaturizer(preset="best"),
+        "cleaner": DataCleaner()
+    }
+
+    fast_config = {
+        "learner": TPOTAdaptor(max_time_mins=30, population_size=50),
+        "reducer": FeatureReducer(reducers=('corr', 'tree')),
+        "autofeaturizer": AutoFeaturizer(preset="fast"),
+        "cleaner": DataCleaner()
+    }
+
+    debug_config = {
+        "learner": TPOTAdaptor(max_time_mins=2,
+                               max_eval_time_mins=1,
+                               population_size=10),
+        "reducer": FeatureReducer(reducers=('corr',)),
+        "autofeaturizer": AutoFeaturizer(preset="fast"),
+        "cleaner": DataCleaner()
+    }
+
+    debug_single_config = {
+        "learner": SinglePipelineAdaptor(model=RandomForestRegressor()),
+        "reducer": FeatureReducer(reducers=('corr',)),
+        "autofeaturizer": AutoFeaturizer(preset="fast"),
+        "cleaner": DataCleaner()
+    }
+
     if preset == "default":
         return default_config
     elif preset == "fast":
         return fast_config
     elif preset == "debug":
         return debug_config
+    elif preset == "debug_single":
+        return debug_single_config
     elif preset == "production":
         return production_config
