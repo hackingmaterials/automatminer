f36f2b84a7cffc856ab03fe8c36c7de264dcb369
ardunn <ardunn@lbl.gov>
2019-02-07 16:38:40 -0800

    prevent log overwrites

--- automatminer/utils/log_tools.py
+++ automatminer/utils/log_tools.py
@@ -2,34 +2,42 @@
 Utils for logging.
 """
 
-import logging
 import os
 import sys
+import logging
+import datetime
 
-LOG_FIT_STR = "fitting"
-LOG_TRANSFORM_STR = "transforming"
-LOG_PREDICT_STR = "predicting"
+AMM_LOG_FIT_STR = "fitting"
+AMM_LOG_TRANSFORM_STR = "transforming"
+AMM_LOG_PREDICT_STR = "predicting"
 
 
-def initialize_logger(name, filepath='.', filename=None, level=None):
+def initialize_logger(name, filepath='.', filebase=None, level=None):
     """Initialize the default logger with stdout and file handlers.
 
     Args:
         name (str): The package name.
         filepath (str): Path to the folder where the log file will be written.
-        filename (str): The log filename.
+        filebase (str): The log filename.
         level (int): The log level. For example logging.DEBUG.
     Returns:
         (Logger): A logging instance with customized formatter and handlers.
     """
     level = level or logging.INFO
-    filename = filename or name + ".log"
 
     logger = logging.getLogger(name)
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
 
-    handler = logging.FileHandler(os.path.join(filepath, filename), mode='w')
+    filebase = filebase or name
+    logpath = os.path.join(filepath, filebase)
+    if os.path.exists(logpath + ".log"):
+        logpath += "_" + datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    logpath += ".log"
+
+    print("LOGPATH is ", logpath)
+
+    handler = logging.FileHandler(logpath, mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
