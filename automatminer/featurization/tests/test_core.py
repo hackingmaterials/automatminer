3d90a388c343585001b2141c132ab4328587da5a
ardunn <ardunn@lbl.gov>
2019-03-02 18:57:54 -0800

    remove metafeatures bc I have a matminer pr in progress for them

--- automatminer/featurization/tests/test_core.py
+++ automatminer/featurization/tests/test_core.py
@@ -295,65 +295,10 @@ class TestAutoFeaturizer(unittest.TestCase):
         self.assertAlmostEqual(df_feats.iloc[3, 0].tolist(),
                                df_cache_feats.iloc[3, 0].tolist())
 
-
-    # todo: Fix test once metaselector is converted over to precheck
-    @unittest.skip(reason="Metaselector needs revamp before re-enabling.")
-    def test_use_metaselector(self):
-        # Test to see if metaselector works for this dataset
-        df = copy.copy(self.test_df.iloc[:self.limit])
-        target = "K_VRH"
-
-        af = AutoFeaturizer(use_metaselector=True)
-        af.fit(df, target)
-
-        self.assertIsNotNone(af.metaselector)
-        dataset_mfs = af.metaselector.dataset_mfs
-        self.assertIn("composition_metafeatures", dataset_mfs.keys())
-        self.assertIn("structure_metafeatures", dataset_mfs.keys())
-        self.assertIsNotNone(dataset_mfs["composition_metafeatures"])
-        self.assertIsNotNone(dataset_mfs["structure_metafeatures"])
-
-        comp_mfs = dataset_mfs["composition_metafeatures"]
-        self.assertEqual(comp_mfs["number_of_compositions"], 5)
-        self.assertAlmostEqual(comp_mfs["percent_of_all_metal"], 0.2)
-        self.assertAlmostEqual(
-            comp_mfs["percent_of_metal_nonmetal"], 0.8)
-        self.assertAlmostEqual(comp_mfs["percent_of_all_nonmetal"], 0.0)
-        self.assertAlmostEqual(
-            comp_mfs["percent_of_contain_trans_metal"], 0.8)
-        self.assertEqual(comp_mfs["number_of_different_elements"], 7)
-        self.assertAlmostEqual(comp_mfs["avg_number_of_elements"], 2.2)
-        self.assertEqual(comp_mfs["max_number_of_elements"], 3)
-        self.assertEqual(comp_mfs["min_number_of_elements"], 1)
-
-        struct_mfs = dataset_mfs["structure_metafeatures"]
-        self.assertEqual(struct_mfs["number_of_structures"], 5)
-        self.assertAlmostEqual(struct_mfs["percent_of_ordered_structures"],
-                               1.0)
-        self.assertAlmostEqual(struct_mfs["avg_number_of_sites"], 7.0)
-        self.assertEqual(struct_mfs["max_number_of_sites"], 12)
-        self.assertEqual(
-            struct_mfs["number_of_different_elements_in_structures"], 7)
-
-        excludes = af.metaselector.excludes
-        self.assertIn("IonProperty", excludes)
-        self.assertIn("Miedema", excludes)
-        self.assertIn("OxidationStates", excludes)
-        self.assertIn("YangSolidSolution", excludes)
-        self.assertIn("TMetalFraction", excludes)
-        self.assertIn("ElectronegativityDiff", excludes)
-        self.assertIn("CationProperty", excludes)
-        self.assertIn("ElectronAffinity", excludes)
-
-        df = af.fit_transform(df, target)
-        ef = ElectronAffinity()
-        ef_feats = ef.feature_labels()
-        self.assertFalse(any([f in df.columns for f in ef_feats]))
-        self.assertFalse(any([f in df.columns for f in ef_feats]))
-
     def tearDown(self):
         if os.path.exists(CACHE_PATH):
             os.remove(CACHE_PATH)
+
 
 if __name__ == '__main__':
     unittest.main()
