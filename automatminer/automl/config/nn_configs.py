d6bed5264ef78f1de5d545be78db971ec4ceae23
ardunn <ardunn@lbl.gov>
2019-04-17 21:33:25 -0700

    comment out configs for now

--- automatminer/automl/config/nn_configs.py
+++ automatminer/automl/config/nn_configs.py
@@ -1,24 +1,24 @@
-"""
-Default arguments and configuration for the Neural Network adaptor (NNAdaptor).
-"""
-
-
-NN_DEFAULT_CONFIG = {
-    "init": "glorot_uniform",
-    "optimizer": "adam",
-    "hidden_layer_sizes": (3, 50, 100),
-    "units": 20,
-    "dropout": 0.5,
-    "batch_spec": ((400, 1024), (100, -1)),
-    "activation": "sigmoid",
-    "input_noise": 0,
-    "use_maxout": False,
-}
-
-init="glorot_uniform", optimizer="adam",
-                 hidden_layer_sizes=2, units=20, dropout=0.5,
-                 show_accuracy=True, batch_spec=((400, 1024), (100, -1)),
-                 activation="sigmoid", input_noise=0., use_maxout=False,
-                 use_maxnorm=False, learning_rate=0.001, stop_early=False,
-                 kfold_splits=2, mode="regression"
-
+# """
+# Default arguments and configuration for the Neural Network adaptor (NNAdaptor).
+# """
+#
+#
+# NN_DEFAULT_CONFIG = {
+#     "init": "glorot_uniform",
+#     "optimizer": "adam",
+#     "hidden_layer_sizes": (3, 50, 100),
+#     "units": 20,
+#     "dropout": 0.5,
+#     "batch_spec": ((400, 1024), (100, -1)),
+#     "activation": "sigmoid",
+#     "input_noise": 0,
+#     "use_maxout": False,
+# }
+#
+# init="glorot_uniform", optimizer="adam",
+#                  hidden_layer_sizes=2, units=20, dropout=0.5,
+#                  show_accuracy=True, batch_spec=((400, 1024), (100, -1)),
+#                  activation="sigmoid", input_noise=0., use_maxout=False,
+#                  use_maxnorm=False, learning_rate=0.001, stop_early=False,
+#                  kfold_splits=2, mode="regression"
+#
