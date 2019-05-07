8bd61312157a67d86b5c5935425001a307a0ac46
ardunn <ardunn@lbl.gov>
2019-05-06 17:00:06 -0700

    outline new DFMLAdaptor

--- automatminer/automl/base.py
+++ automatminer/automl/base.py
@@ -3,8 +3,14 @@ Base classes for automl.
 """
 
 import abc
+from typing import List, Dict
+
+import pandas as pd
+from sklearn.base import BaseEstimator
 
 from automatminer.base import DFTransformer
+from automatminer.utils.log import AMM_LOG_PREDICT_STR, log_progress
+from automatminer.utils.pkg import AutomatminerError, check_fitted
 
 
 class DFMLAdaptor(DFTransformer):
@@ -16,33 +22,19 @@ class DFMLAdaptor(DFTransformer):
     and @set_fitted if necessary!
     """
 
+    @property
     @abc.abstractmethod
-    def predict(self, df, target):
+    def fitted_target(self) -> str:
         """
-        Using a fitted object, use the best model available to transform a
-        dataframe not containing the target to a dataframe containing the
-        predicted target values.
-
-        Analagous to DFTransformer.transform
-
-        Args:
-            df (pandas.DataFrame): The dataframe to-be-predicted
-            target: The target metric to be predicted. The output column for
-                the data will be "predicted {target}".
-
+        The target (a string) on which the adaptor was fit on.
         Returns:
-            (pandas.DataFrame): The dataframe updated with predictions of the
-                target property.
-
+            (str): The fitted target label.
         """
         pass
 
-    def transform(self, df, target):
-        return self.predict(df, target)
-
     @property
     @abc.abstractmethod
-    def features(self):
+    def features(self) -> List:
         """
         The features being used for machine learning.
 
@@ -53,7 +45,7 @@ class DFMLAdaptor(DFTransformer):
 
     @property
     @abc.abstractmethod
-    def ml_data(self):
+    def ml_data(self) -> Dict:
         """
         The raw ML-data being passed to the backend.
 
@@ -65,7 +57,7 @@ class DFMLAdaptor(DFTransformer):
 
     @property
     @abc.abstractmethod
-    def best_pipeline(self):
+    def best_pipeline(self) -> BaseEstimator:
         """
         The best pipeline returned by the automl backend. Should implement fit
         and predict methods and be able to make predictions.
@@ -79,10 +71,63 @@ class DFMLAdaptor(DFTransformer):
     @abc.abstractmethod
     def backend(self):
         """
-        The raw, fitted backend object, if it exists.
+        The raw, fitted backend object, if it exists. Does not need to directly
+        implement fit and predict methods.
 
         Returns:
             Backend object (e.g., TPOTClassifier)
 
         """
         pass
+
+    @log_progress(AMM_LOG_PREDICT_STR)
+    @check_fitted
+    def predict(self, df: pd.DataFrame, target: str) -> pd.DataFrame:
+        """
+        Predict the target property of materials given a df of features. This
+        base method is widely applicable across many kinds of DFMLAdaptors.
+
+        The predictions are appended to the dataframe in a column called:
+            "{target} predicted"
+
+        Args:
+            df (pandas.DataFrame): Contains all features needed for ML (i.e.,
+                all features contained in the training dataframe.
+            target (str): The property to be predicted. Should match the target
+                used for fitting. May or may not be present in the argument
+                dataframe.
+
+        Returns:
+            (pandas.DataFrame): The argument dataframe plus a column containing
+                the predictions of the target.
+
+        """
+        if target != self.fitted_target:
+            raise AutomatminerError(
+                "Argument dataframe target ({}) is different from the fitted "
+                "dataframe target! ({})".format(target, self.fitted_target)
+            )
+        elif not all([f in df.columns for f in self.features]):
+            not_in_model = [f for f in self.features if
+                            f not in df.columns]
+            not_in_df = [f for f in df.columns if f not in self.features]
+            raise AutomatminerError(
+                "Features used to build model are different from df columns! "
+                "Features located in model not located in df: \n{} \n "
+                "Features located in df not in model: \n{}"
+                "".format(not_in_df, not_in_model))
+        else:
+            X = df[self.features].values  # rectify feature order
+            y_pred = self.backend.predict(X)
+            df[target + " predicted"] = y_pred
+
+            log_msg = "Prediction finished successfully."
+            try:
+                self.logger.info(self._log_prefix + log_msg)
+            except AttributeError:
+                pass
+            return df
+
+    def transform(self, df: pd.DataFrame, target: str) -> pd.DataFrame:
+        return self.predict(df, target)
+
