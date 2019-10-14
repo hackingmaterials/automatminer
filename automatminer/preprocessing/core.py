8309edab141b3e37d7f3a128c919765f248ff957
ardunn <ardunn@lbl.gov>
2019-10-14 13:49:55 -0700

    fix #199

--- automatminer/preprocessing/core.py
+++ automatminer/preprocessing/core.py
@@ -94,7 +94,8 @@ class DataCleaner(DFTransformer, LoggableMixin):
         self.fitted_df = None
         self.fitted_target = None
         self.dropped_samples = None
-        self.max_problem_col_warning_threshold = 0.1
+        self.max_problem_col_warning_threshold = 0.3
+        self.warnings = []
         super(DataCleaner, self).__init__()
 
     @property
@@ -228,16 +229,16 @@ class DataCleaner(DFTransformer, LoggableMixin):
             n_total_cols = df.shape[1]
             problem_col_frac = n_problem_cols/n_total_cols
             if problem_col_frac >= self.max_problem_col_warning_threshold:
-                self.logger.error(
-                    self._log_prefix +
-                    f"Fraction {problem_col_frac} of all columns had nan "
-                    f"percentages exceeding the nan threshold of "
-                    f"{self.max_na_frac}. It is likely featurization was not "
-                    f"effective. Please ensure your featurization input "
-                    f"objects (e.g., compositions) are applicable for "
-                    f"AutoFeaturizer! Examine your input data and pipeline "
+                warning = \
+                    f"Fraction {problem_col_frac} of all columns had nan " \
+                    f"percentages exceeding the nan threshold of " \
+                    f"{self.max_na_frac}. It is likely featurization was not " \
+                    f"effective. Please ensure your featurization input " \
+                    f"objects (e.g., compositions) are applicable for " \
+                    f"AutoFeaturizer! Examine your input data and pipeline " \
                     f"in detail."
-                                    )
+                self.logger.error(self._log_prefix + warning)
+                self.warnings.append(warning)
             if self.feature_na_method == "drop":
                 df = df.dropna(axis=1, thresh=threshold)
             else:
