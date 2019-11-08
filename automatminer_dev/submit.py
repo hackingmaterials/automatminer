984005ddaaac2762f6d3567cbe5d0b27afa009ee
ardunn <ardunn@lbl.gov>
2019-11-08 15:23:34 -0800

    update dev

--- automatminer_dev/submit.py
+++ automatminer_dev/submit.py
@@ -15,7 +15,7 @@ Running benchmarks
 
 if __name__ == "__main__":
 
-    N_JOBS = 10
+    N_JOBS = 50
 
     pipe_config = {
         "learner_name": "TPOTAdaptor",
@@ -26,7 +26,7 @@ if __name__ == "__main__":
             "max_eval_time_mins": 20,
             "population_size": 200,
             #todo: change this back
-            # "memory": "auto",
+            "memory": "auto",
             "n_jobs": N_JOBS,
         },
         # "reducer_kwargs": {"reducers": ("corr",)},
@@ -79,9 +79,9 @@ if __name__ == "__main__":
     ]
 
     from automatminer_dev.config import EXPT_IS_METAL, EXPT_GAP, MP_E_FORM
-    worker = "lrc"
-    # wf = wf_benchmark(worker, pipe_config, **EXPT_IS_METAL, cache=True, tags=tags)
-    wf = wf_benchmark(worker, pipe_config, **EXPT_GAP, cache=True, tags=tags)
+    worker = "cori"
+    wf = wf_benchmark(worker, pipe_config, **MP_E_FORM, cache=True, tags=tags, prepend_name="DEBUG_+NPROC")
+    # wf = wf_benchmark(worker, pipe_config, **EXPT_GAP, cache=True, tags=tags)
     # wf = wf_benchmark(worker, pipe_config_debug, **EXPT_IS_METAL, cache=True, tags=tags)
     # wf = wf_benchmark(worker, pipe_config_debug, **EXPT_GAP, cache=True, tags=tags)
 
