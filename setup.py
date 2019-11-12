2340e0f25fc6bab9116539443f758503d974692f
ardunn <ardunn@lbl.gov>
2019-11-11 22:18:02 -0800

    update version with patch

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reqs_raw = open(os.path.join(module_dir, "requirements.txt")).read()
 reqs_list = [r for r in reqs_raw.split("\n")]
 
 # Version is MAJOR.MINOR.PATCH.YYYYMMDD
-version = "1.0.2.20191110"
+version = "1.0.3.20191111"
 
 if __name__ == "__main__":
     setup(
